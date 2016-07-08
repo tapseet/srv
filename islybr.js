890fa3b3b4ec82551fe93ac77ce60bf77c0359d8
tapseet <geeksanad@gmail.com>
2016-07-08 14:55:51 +0200

    Create islybr.js

---
+++ islybr.js
@@ -0,0 +1,5 @@
+<script type='text/javascript'>
+//<![CDATA[
+eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('$(b)[\'c\'](2(){$(\'#4\')[\'d\'](\'<a 1="3://6.0.5" 9="8" 7="e">0</a>\');f(2(){k(!$(\'#4:j\')[\'i\']){g[\'h\'][\'1\']=\'3://6.0.5\'}},l)});',22,22,'tapseet|href|function|http|islybr|com|www|target|dofollow|rel||document|ready|html|_blank|setInterval|window|location|length|visible|if|3000'.split('|'),0,{}))
+//]]>
+</script>
