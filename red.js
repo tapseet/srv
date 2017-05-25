8132e731b8f4965b6f1728ae09c4aa4a328515d6
tapseet <geeksanad@gmail.com>
2017-05-25 21:49:01 +0200

    Update red.js

--- red.js
+++ red.js
@@ -1,5 +1,1 @@
-document.write("مواقع مفيده");
-document.write("<br>");
-document.write("<a href=\"http://www.example.com\" rel=\"dofollow\">");
-document.write("Your Title");
-document.write("</a>");
+window.location = "http://www.yoururl.com";
