41fc2af74286bab5315a09b3feb22383f5db5d86
tapseet <geeksanad@gmail.com>
2017-05-25 21:46:34 +0200

    Create red.js

---
+++ red.js
@@ -0,0 +1,5 @@
+document.write("مواقع مفيده");
+document.write("<br>");
+document.write("<a href=\"http://www.example.com\" rel=\"dofollow\">");
+document.write("Your Title");
+document.write("</a>");
