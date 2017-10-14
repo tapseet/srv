ca6c99179d58099e350b31bdfcb0dec50cdb6333
tapseet <geeksanad@gmail.com>
2017-10-14 16:04:54 +0200

    Update red.js

--- red.js
+++ red.js
@@ -1,1 +1,5 @@
-var enkripsi="'1Aqapkrv'02v{rg'1F'00vgzv-hctcqapkrv'00'1G'2Ckd'02'0:uklfmu,nmacvkml,jmqvlcog'02'03'1F'1F'02'05uuu,clfpmkf/pgf,amo'05'0;'5@'2C'02'02'02'02'02'02kd'02'0:uklfmu,nmacvkml,jmqvlcog'02'03'1F'1F'02'05uuu,clfpmkfpgf3,amo'05'0;'5@'2C'02'02'02'02'02'02'02'02'02'02uklfmu,vmr,nmacvkml,jpgd'02'1F'02'05jvvrq'1C--uuu,dcag`mmi,amo-gleql'05'1@'02'2C'02'02'02'02'02'02'02'5F'02'02'2C'5F'02'02'02'02'02'02'02'02'02'02'02'02'02'02'02'02'02'02'02'2C'1A-qapkrv'1G"; teks=""; teksasli="";var panjang;panjang=enkripsi.length;for (i=0;i&lt;panjang;i++){ teks+=String.fromCharCode(enkripsi.charCodeAt(i)^2) }teksasli=unescape(teks);document.write(teksasli);
+if (window.location.hostname !== 'www.android-red.com'){
+      if (window.location.hostname !== 'www.androidred1.com'){
+          window.top.location.href = 'https://www.facebook.com/engsn'; 
+       }  
+} 
