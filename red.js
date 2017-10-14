55a70fc663170cb6a237f314ccd30621bff0e9a4
tapseet <geeksanad@gmail.com>
2017-10-14 16:32:07 +0200

    Update red.js

--- red.js
+++ red.js
@@ -1,7 +1,1 @@
-if (window.location.hostname !== 'www.android-red.com'){
-    if (window.location.hostname !== 'www.androidred1.com'){
-         if (window.location.hostname !== 'www.hampdf.com'){
-          window.top.location.href = 'http://bit.ly/protectmin'; 
-        } 
-    }  
-} 
+
