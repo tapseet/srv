1acdc5d7c599f0f189510d183e792fc533ae6c7a
tapseet <geeksanad@gmail.com>
2017-10-14 16:11:50 +0200

    Update red.js

--- red.js
+++ red.js
@@ -1,5 +1,7 @@
 if (window.location.hostname !== 'www.android-red.com'){
-      if (window.location.hostname !== 'www.androidred1.com'){
+    if (window.location.hostname !== 'www.androidred1.com'){
+         if (window.location.hostname !== 'www.hampdf.com'){
           window.top.location.href = 'https://www.facebook.com/engsn'; 
-       }  
+        } 
+    }  
 } 
